2993072d8846b919df8f3cda71c0d3bd6dc6c586
Masoud Omran <mo@t24defence.com>
2018-10-03 09:38:17 -0400

    initital commit

---
+++ server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dotnettest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There!");
+            var name= Console.ReadLine();
+            
+            Console.WriteLine("Hello {0}!!!", name);
+        }
+    }
+}
